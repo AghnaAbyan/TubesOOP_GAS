ab15d60939efd3b13a7cf9178f737cbb4c2a4be1
Rezapu Obed <obed.ubaidillah@gmail.com>
2023-04-04 09:19:46 +0700

    another random fix

--- src/Utils/Utils.hpp
+++ src/Utils/Utils.hpp
@@ -2,18 +2,19 @@
 #include <random>
 #include <vector>
 #include <algorithm>
+#include <ctime>
 
 using namespace std;
 
 class Utils{
     public:
-        static mt19937 rng(){
-            random_device rd;
-            return mt19937(rd());
+        static void rng_seed(){
+            srand((unsigned int)time(NULL));
         }
 
         template<class T>
         static void shuffle(vector<T> &v){
-            std::shuffle(v.begin(), v.end(), Utils::rng());
+            rng_seed();
+            random_shuffle(v.begin(), v.end());
         }
 };
